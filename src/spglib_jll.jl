41936ad9b44ba55aa1800b27660e8effdafc6996
jlbuild <juliabuildbot@gmail.com>
2025-12-31 21:29:20 +0000

    spglib_jll build 2.7.0+0

--- src/spglib_jll.jl
+++ src/spglib_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("spglib")
-JLLWrappers.@generate_main_file("spglib", UUID("ac4a9f1e-bdb2-5204-990c-47c8b2f70d4e"))
+JLLWrappers.@generate_main_file("spglib", Base.UUID("ac4a9f1e-bdb2-5204-990c-47c8b2f70d4e"))
 end  # module spglib_jll
